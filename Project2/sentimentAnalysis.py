0e56bada4e922063baf233583dc25956a2d66b44
cargon21 <sytnerc@gmail.com>
2021-03-17 11:50:15 -0400

    Complete Project

--- Project2/sentimentAnalysis.py
+++ Project2/sentimentAnalysis.py
@@ -1,66 +1,69 @@
-# Programmers: Jordan DeGiacomo & Carson Synter
-# Last DOM: 3/16/2020
-# Course: CMPSC 310
-# Purpose: Write a program that shall calculate word sentiment level, based on the user reviews from the Yelp academic dataset
-
-# Game plan: 1) Load JSON file (small set), final run will have all reviews
-#            2) Extract all review text and star ratings
-#            3) Break each review into individuals words using NLTK
-#            4) Lemmatize the words
-#            5) Filter out stop words and words that are not in the words corpus
-#            6) For each lemma calculate its star rating, if lemma is used in less than 10 reviews discard it
-#            7) Save 500 most negative lemmas and 500 most positive lemmas and their respective sentiment levels in a one two-column CSV file
-#               Lemmas in column one and levels in the second, sorted in decending order of sentiment analysis
+#----------------------------------------------------------------------------------
+# - Programmers: Jordan DeGiacomo & Carson Synter
+# - Last DOM: 3/16/2020
+# - Course: CMPSC 310
+# - Purpose: Write a program that shall calculate word sentiment level, based on 
+#   the user reviews from the Yelp academic dataset
+# - Final Execution time: ~ 2.5 minutes
+#----------------------------------------------------------------------------------
 
 import json
 import csv
 import nltk
-from collections import Counter
+import sys
 from nltk.corpus import stopwords
 from nltk.corpus import words
+from statistics import mean 
 
-# open sing json
-with open ("../../yelp_academic_dataset_review_small.json") as yelpFile:
-    real_yelp_data = json.load(yelpFile)
+# Open the JSON file
+try:
+    with open ("../../yelp_academic_dataset_review_small.json") as yelpFile:
+        yelp_data = json.load(yelpFile)
+except Exception as e:
+    print(f"there was an error opening the file: {e})")
+    sys.exit()
 
-# create some sample yelp data to experiment on
-yelp_data = []
-index = 0
-for i in real_yelp_data:
-    if index > 3:
-        break
-    yelp_data.append(i)
-    index+= 1
+# Sets of english words and stopwords
+stopwords = set(stopwords.words('english'))
+corpus = set(words.words("en"))
 
-
-wnl = nltk.WordNetLemmatizer()
-reviews_ratings = []
+wnl = nltk.WordNetLemmatizer() #  Word lemmatizer
+reviews_ratings = {}
 
 for review in yelp_data:
-    tokenize = nltk.word_tokenize(review['text'])
     
-    lemmatize = [wnl.lemmatize(w) for w in tokenize]
-            
-    filter_words = [i for i in lemmatize if i not in set(stopwords.words('english')) and i.isalnum()]
+    # Lammatize each review
+    lemmatize = [wnl.lemmatize(w) for w in nltk.word_tokenize(review['text'].lower())]
     
-    reviews_ratings.append([review['stars'], filter_words])
+    # Filter the review
+    filter_words = set([i for i in lemmatize if i not in stopwords and i.isalnum() and i in corpus])
     
+    for word in filter_words:
+        if word not in reviews_ratings.keys(): # New entry if non existent
+            reviews_ratings[word] = [review['stars']]
+        else:
+            reviews_ratings[word].append(review['stars']) # Add the review to the word
+
+# Remove lemmas used in fewer than 10 ratings                                     
+for key, value in reviews_ratings.copy().items():
+    if len(value) < 10:
+        del reviews_ratings[key]
+    else:
+        reviews_ratings[key] = mean(value)
+
+final = list(sorted(reviews_ratings.items(), key=lambda item: item[1], reverse = True))
+
+file_name = "reviews.csv"
+
+# Open and write to the CSV file
+with open(file_name, 'w') as csvfile:
+       writer = csv.writer(csvfile)
+       writer.writerow(("Word", "AVG Rating"))
+       
+       writer.writerow(("Word", "Highest Ratings"))
+       writer.writerows(final[0:500])
+       
+       writer.writerow(("Word", "Lowest Ratings"))
+       writer.writerows(final[-501:-1])
     
-    
-    
-    
-# reviews_ratings = [[review['stars'], nltk.word_tokenize(review['text'])] for review in yelp_data]
-
-# wnl = nltk.WordNetLemmatizer()
-
-
-
-# len([word for word in tokens if word not in set(stopwords.words('english')) and word.isalnum()])
-
-# wnl = nltk.WordNetLemmatizer()
-
-# a = reviews_ratings[0][1]
-# li = [wnl.lemmatize(w) for w in a]
-
-# for i in range(150): 
-#     print(a[i] + " ---- " + li[i], str(a[i] == li[i]))
+print("Program executed successfully :)")
